5ffd8d0fe0d18236094bd563d0cce5cb00eb20b1
Douglas Domenico Bollella <douglas.bollella@xpi.com.br>
2020-09-25 14:58:18 -0300

    Adicionar arquivos de projeto.

---
+++ RestApiUsingK6/Controllers/WeatherForecastController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RestApiUsingK6.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<int> Get()
+        {
+            var randomizer = new Random();
+            int randomDelay = randomizer.Next(0, 5);
+
+            await Task.Delay(TimeSpan.FromSeconds(randomDelay));
+
+            return randomDelay;
+        }
+    }
+}
